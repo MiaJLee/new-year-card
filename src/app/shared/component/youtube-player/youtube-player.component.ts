53031ddecb0419d21efb247bea6933dc5c039b35
MiaJLee <angella555@naver.com>
2022-12-30 17:35:48 +0900

    fix: youtube player mobile css fix

--- src/app/shared/component/youtube-player/youtube-player.component.ts
+++ src/app/shared/component/youtube-player/youtube-player.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Music } from '../../../app.models';
 import { getUrlParameter, isMobile as _isMobile } from '../../../app.utils';
 import { MUSICS } from '../../../app.value';
 
 @Component({
-	selector: 'app-youtube-player',
-	templateUrl: './youtube-player.component.html',
+  selector: 'app-youtube-player',
+  templateUrl: './youtube-player.component.html',
 })
 export class YoutubePlayerComponent implements OnInit {
   @Input() musicId?: number;
@@ -14,23 +13,25 @@ export class YoutubePlayerComponent implements OnInit {
   musics = MUSICS;
 
   get isMobile(): boolean {
-  	return _isMobile();
+    return _isMobile();
   }
 
+  constructor() {}
+
   ngOnInit() {
-  	if (!this.apiLoaded) {
-  		// This code loads the IFrame Player API code asynchronously, according to the instructions at
-  		// https://developers.google.com/youtube/iframe_api_reference#Getting_Started
-  		const tag = document.createElement('script');
-  		tag.src = 'https://www.youtube.com/iframe_api';
-  		document.body.appendChild(tag);
-  		this.apiLoaded = true;
-  	}
+    if (!this.apiLoaded) {
+      // This code loads the IFrame Player API code asynchronously, according to the instructions at
+      // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
 
-  	if (!this.musicId) return;
-  	const playMusic = this.musics.find(({ id }) => id === this.musicId);
+    if (!this.musicId) return;
+    const playMusic = this.musics.find(({ id }) => id === this.musicId);
 
-  	if (!playMusic) return;
-  	this.videoId = getUrlParameter(playMusic?.youtubeLink, 'v');
+    if (!playMusic) return;
+    this.videoId = getUrlParameter(playMusic?.youtubeLink, 'v');
   }
 }
