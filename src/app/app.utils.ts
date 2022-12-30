39f2ea08c4c5ece046da1d53bab4f68ec584a4fc
MiaJLee <angella555@naver.com>
2022-12-30 17:33:50 +0900

    feat: korean suffix utils

--- src/app/app.utils.ts
+++ src/app/app.utils.ts
@@ -23,3 +23,46 @@ export function focusOnWriting() {
 		}, 0);
 	}
 }
+
+export function koreanSuffix(text: string): string {
+	const KR_SUFFIX = ['을(를)', '이(가)', '와(과)'];
+	let fixedText = text;
+
+	KR_SUFFIX.forEach((s) => {
+		if (text.indexOf(s) === -1) {
+			return;
+		}
+
+		const finalCharCode = text.charCodeAt(text.indexOf(s) - 1);
+		const finalConsonantCode = (finalCharCode - 44032) % 28;
+		const isFinalCharBatchim = finalConsonantCode !== 0;
+
+		if (isFinalCharBatchim) {
+			switch (s) {
+			case '을(를)':
+				fixedText = fixedText.replace('을(를)', '을');
+				break;
+			case '이(가)':
+				fixedText = fixedText.replace('이(가)', '이');
+				break;
+			case '와(과)':
+				fixedText = fixedText.replace('와(과)', '와');
+				break;
+			}
+		} else {
+			switch (s) {
+			case '을(를)':
+				fixedText = fixedText.replace('을(를)', '를');
+				break;
+			case '이(가)':
+				fixedText = fixedText.replace('이(가)', '가');
+				break;
+			case '와(과)':
+				fixedText = fixedText.replace('와(과)', '과');
+				break;
+			}
+		}
+	});
+
+	return fixedText;
+}
